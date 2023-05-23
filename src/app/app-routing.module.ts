907a3b8a4b7367a370a8ab6486636a47dd8eb856
Jhosep2022 <jose.condori.r@ucb.edu.bo>
2023-05-23 16:24:22 -0400

    Agregacion de las rutas y creacion de las paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegistroPageComponent } from './registro-page/registro-page.component';
+import { VentaPageComponent } from './venta-page/venta-page.component';
+import { ListaVentaPageComponent } from './lista-venta-page/lista-venta-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent },	
+  { path: 'login', component: LoginPageComponent },
+  { path: 'registro', component: RegistroPageComponent },
+  { path: 'venta', component: VentaPageComponent },
+  { path: 'lista-venta', component: ListaVentaPageComponent },
+  { path: 'contact', component: ContactPageComponent },
+  { path: 'product', component: ProductPageComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
